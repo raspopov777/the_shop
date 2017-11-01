c29a0ae729837bb4a15166ec9fa0fc6ada51ebb4
Sergey Raspopov <raspopov.sergey.na@gmail.com>
2017-11-01 23:01:07 +0300

    add form, radio, addEventListener

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,49 +1,12 @@
-/*eslint-disable */
-function color(c){switch(c){
-        case 'white': {
-            document.getElementById('big').src = 'img/tshirt_white.jpg';
-            document.getElementById('color_w').classList.add('active');
-            document.getElementById('color_g').classList.remove('active');
-            document.getElementById('color_y').classList.remove('active');
-            break;
-        }
-        case 'yellow': {
-            document.getElementById('big').src = 'img/tshirt_yellow.jpg';
-            document.getElementById('color_y').classList.add('active');
-            document.getElementById('color_g').classList.remove('active');
-            document.getElementById('color_w').classList.remove('active');
-            break;
-        }
-        case 'green': {
-            document.getElementById('big').src = 'img/tshirt_green.jpg';
-            document.getElementById('color_g').classList.add('active');
-            document.getElementById('color_w').classList.remove('active');
-            document.getElementById('color_y').classList.remove('active');
-            break;
-        }
-    }
-}
 
-function size(c){
-    switch(c){
-        case 'S': {
-            document.getElementById('size-s').classList.add('active');
-            document.getElementById('size-m').classList.remove('active');
-            document.getElementById('size-l').classList.remove('active');
-            break;
-
-        }
-        case 'M': {
-            document.getElementById('size-m').classList.add('active');
-            document.getElementById('size-s').classList.remove('active');
-            document.getElementById('size-l').classList.remove('active');
-            break;
-        }
-        case 'L': {
-            document.getElementById('size-l').classList.add('active');
-            document.getElementById('size-s').classList.remove('active');
-            document.getElementById('size-m').classList.remove('active');
-            break;
-        }
+document.addEventListener('click',function (event) {
+    if(event.target.classList.contains('rectangle_white')){
+        document.getElementById('big').src = 'img/tshirt_white.jpg';
     }
-}
+    if(event.target.classList.contains('rectangle_yellow')){
+        document.getElementById('big').src = 'img/tshirt_yellow.jpg';
+    }
+    if(event.target.classList.contains('rectangle_green')){
+        document.getElementById('big').src = 'img/tshirt_green.jpg';
+    }
+});
